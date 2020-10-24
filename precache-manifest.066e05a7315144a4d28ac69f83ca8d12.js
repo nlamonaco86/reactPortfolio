aa62619ca95bd0e61a7598cc65f322cb9ee232a3
Nicholas <nlamonaco86@gmail.com>
2020-10-23 22:14:49 -0400

    Updates

---
+++ precache-manifest.066e05a7315144a4d28ac69f83ca8d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24865edb4f5dab6be3f8a3d662df869",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "d07d38cec8d6527545c5",
+    "url": "/reactPortfolio/static/css/main.b94482c6.chunk.css"
+  },
+  {
+    "revision": "f8db00ed32024b0152e8",
+    "url": "/reactPortfolio/static/js/2.b3ffe55c.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/reactPortfolio/static/js/2.b3ffe55c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07d38cec8d6527545c5",
+    "url": "/reactPortfolio/static/js/main.ef37f474.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  }
+]);
